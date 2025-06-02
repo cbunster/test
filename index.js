997d55c20879913741338a5328aa9e2ad39db007
cbunster <cbunster@users.noreply.github.com>
2025-06-02 17:04:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -103,5 +103,5 @@ app.post("/webhook", (req, res) => { //i want some
 });
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello Ansh this is webhook setup");
+    res.status(200).send("Hello Christopher this is webhook setup");
 });
